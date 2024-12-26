14146be0b2ad59027a554cf26de34e83d94f8ce1
Brian Larson <brian@brianlarson.com>
2024-12-25 23:43:02 -0600

    Move data and pass down as prop from app to list

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,55 @@
 import { useState } from "react";
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 function getTitle(title) {
   return title;
 }
 
 const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
   return (
     <div>
       <h1>{getTitle("Hello React")}</h1>
 
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
 
-      <List />
+      <List list={stories} />
     </div>
   );
 };
 
-const List = () => {
+const Search = () => {
+  return (
+    <div>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
+    </div>
+  );
+};
+
+const List = (props) => {
   return (
     <ul>
-      {list.map(function (item) {
+      {props.list.map(function (item) {
         return (
           <li key={item.objectID}>
             <span>
