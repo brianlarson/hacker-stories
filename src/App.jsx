93c427820d370a7c633eecdb482517edc4d89758
Brian Larson <brian@brianlarson.com>
2024-12-25 23:38:45 -0600

    Render a list as new component in app

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,31 @@ const App = () => {
 
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" />
+
+      <hr />
+
+      <List />
     </div>
+  );
+};
+
+const List = () => {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.title}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
